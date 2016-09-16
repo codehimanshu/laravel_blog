190aa25a884912f0fa6ecf7ea0acc3c35865a2fc
codehimanshu <himanshuagrawal1998@gmail.com>
2016-09-16 07:32:58 +0530

    Post Creation Added

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Blog</title>
+		<link rel="stylesheet prefetch" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			@yield('content')
+		</div>
+	</body>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"</
+</html>
